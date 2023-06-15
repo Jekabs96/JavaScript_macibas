65fabdd657cabd6a6a68655620adbeee1dd2d8f9
Jekabs96 <jekabs.brauers@gmail.com>
2023-06-15 21:16:54 +0300

    pabeigsts viss līdz plūsma

---
+++ scripts/exercise.js
@@ -0,0 +1,61 @@
+
+let vards1 = "Jānītis";
+let vecums1 = 20;
+let irStudents1 = true;
+
+console.log(vards1, vecums1, irStudents1);
+
+let augli = ["Ābols", 20, true];
+//console.log(augli)
+
+for(let i = 0; i<augli.length;i++){
+    console.log(augli[i]);
+}
+
+// let activeElement = document.getElementById("doWhile");
+// activeElement.addEventListener("click", function());
+
+let persona = {
+    vards: "Jekabs",
+    vecums: 26,
+    irStudents: true
+}
+console.log(persona);
+
+function manaFunkcija() {
+    const x = 5;
+    console.log(x);
+  }
+manaFunkcija();
+  
+
+  let y = 10;
+  function uzd5(){
+    console.log(y)
+  }
+  uzd5();
+
+
+  function uzd6(p) {
+     p = "pabeigts"; 
+    console.log(p)
+  }
+uzd6();
+console.log(uzd6(4));
+
+let h = 15;
+function uzd7(){
+    const h = 20;
+  console.log(h)
+}
+uzd7();
+
+function uzd8 (a, b) {
+c = a + b,
+console.log(c)
+}
+uzd8(1,2);
+
+let num = 10;
+num+=5;
+console.log(num);
